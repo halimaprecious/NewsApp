6005a5a823a13c113e2be7dce3d5469bc689564c
halima precious <halimaprecious3@gmail.com>
2022-05-02 10:25:44 +0300

    Adds bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap()
 
 
 def create_app(config_name):
@@ -10,7 +12,9 @@ def create_app(config_name):
     #app configurations
     app.config.from_object(config_options[config_name])
 
-    
+    #initializing flask extention
+    bootstrap.init_app(app)
+
     #registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
