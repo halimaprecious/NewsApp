14bd7147a9d433d226aac09c74178fb4771bad24
halima precious <halimaprecious3@gmail.com>
2022-05-02 14:33:16 +0300

    ft:Adds articles route

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,17 @@
 
 from flask import render_template
 from . import main
-from ..request import get_source
+from ..request import get_source,get_news
 
 @main.route('/')
 def source():
     news_source = get_source()
     title = 'QuickUpdates'
     return render_template('source.html', title = title,sources = news_source)
+
+@main.route('/articles')
+def articles():
+    news = get_news()
+    title = 'HotTeaNews'
+
+    return render_template('news.html',articles = news ,title = title)
