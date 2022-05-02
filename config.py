c359426d0b821bdb1e23cc90ef0f38801c52b175
halima precious <halimaprecious3@gmail.com>
2022-05-02 10:35:19 +0300

    Changes base url

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ import os
 
 class Config:
     '''General configuration parent class'''
-    NEWS_API_BASE_URL = 'https://newsapi.org/v1/articles/{}?api_key={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=0cff81b85884415aa6f77fb081c448f8'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
-    SECRET_KEY = os.environ.get('SECRET_KAY')
 
 
 class ProdConfig(Config):
